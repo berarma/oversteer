caa658a4a1b37b402012c867f1c94ffa0537e79f
Bernat Arlandis <berarma@hotmail.com>
2019-02-27 00:24:24 +0100

    Add Spanish translation

--- oversteer.py
+++ oversteer.py
@@ -4,6 +4,8 @@ import argparse
 import configparser
 import gi
 import glob
+import locale
+from locale import gettext as _
 import signal
 import subprocess
 import sys
@@ -22,6 +24,7 @@ class OversteerUi:
         self.saved_profile = None
 
         self.builder = Gtk.Builder()
+        self.builder.set_translation_domain('oversteer')
         self.builder.add_from_file('ui.glade')
         self.builder.connect_signals(self)
         self.window = self.builder.get_object('main_window')
@@ -171,36 +174,41 @@ class OversteerUi:
 
         self.device_combobox.set_active_id(device_id)
 
-if len(sys.argv) == 1:
-    gi.require_version('Gtk', '3.0')
-    from gi.repository import Gtk
-    OversteerUi()
-else:
-    parser = argparse.ArgumentParser(description="Oversteer - Steering Wheel Manager")
-    parser.add_argument('device_id', nargs='?', help="Device id")
-    parser.add_argument('--list', action='store_true', help="List devices")
-    parser.add_argument('--mode', help="Wheel mode")
-    parser.add_argument('--range', type=int, help="Rotation range")
-    parser.add_argument('--combine-pedals', dest='combine_pedals', default=None, action='store_true', help="combine pedals")
-    parser.add_argument('--no-combine-pedals', dest='combine_pedals', action='store_false')
+if __name__ == "__main__":
+    locale.bindtextdomain('oversteer', 'po')
+    locale.textdomain('oversteer')
+    locale.setlocale(locale.LC_ALL, '')
 
-    args = parser.parse_args()
+    if len(sys.argv) == 1:
+        gi.require_version('Gtk', '3.0')
+        from gi.repository import Gtk
+        OversteerUi()
+    else:
+        parser = argparse.ArgumentParser(description=_("Oversteer - Steering Wheel Manager"))
+        parser.add_argument('device_id', nargs='?', help=_("Device id"))
+        parser.add_argument('--list', action='store_true', help=_("list connected devices"))
+        parser.add_argument('--mode', help=_("set the compatibility mode"))
+        parser.add_argument('--range', type=int, help=_("set the rotation range"))
+        parser.add_argument('--combine-pedals', dest='combine_pedals', default=None, action='store_true', help=_("combine pedals"))
+        parser.add_argument('--no-combine-pedals', dest='combine_pedals', action='store_false')
 
-    wheels = Wheels()
+        args = parser.parse_args()
 
-    device_id = args.device_id
+        wheels = Wheels()
 
-    if args.list != None:
-        devices = wheels.get_devices()
-        print("Devices found:")
-        for key, name in devices:
-            print("    {} ({})".format(name, key))
-    if args.mode != None:
-        wheels.set_mode(device_id, args.mode)
-    if args.range != None:
-        wheels.set_range(device_id, args.range)
-    if args.combine_pedals != None:
-        wheels.set_combine_pedals(device_id, args.combine_pedals)
+        device_id = args.device_id
 
-    sys.exit(0)
+        if args.list != None:
+            devices = wheels.get_devices()
+            print(_("Devices found:"))
+            for key, name in devices:
+                print("    {} ({})".format(name, key))
+        if args.mode != None:
+            wheels.set_mode(device_id, args.mode)
+        if args.range != None:
+            wheels.set_range(device_id, args.range)
+        if args.combine_pedals != None:
+            wheels.set_combine_pedals(device_id, args.combine_pedals)
+
+        sys.exit(0)
 
