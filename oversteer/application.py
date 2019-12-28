436e60988174762c8390f79c4957015a40ff4f82
Bernat Arlandis <berarma@hotmail.com>
2019-12-28 14:08:29 +0100

    Loading of profiles in CLI

--- oversteer/application.py
+++ oversteer/application.py
@@ -1,7 +1,9 @@
 import argparse
+import configparser
 from locale import gettext as _
 import os
 import sys
+from xdg.BaseDirectory import *
 
 class Application:
     def __init__(self, version, pkgdatadir):
@@ -22,6 +24,7 @@ class Application:
             parser.add_argument('--no-combine-pedals', dest='combine_pedals', action='store_false')
             parser.add_argument('--autocenter-strength', type=int, help=_("set the autocenter strength"))
             parser.add_argument('--ff-gain', type=int, help=_("set the FF gain"))
+            parser.add_argument('--profile', help=_("load settings from a profile"))
 
             args = parser.parse_args(argv[1:])
 
@@ -48,6 +51,19 @@ class Application:
                 wheels.set_autocenter_strength(device_id, args.autocenter_strength)
             if args.ff_gain != None:
                 wheels.set_ff_gain(device_id, args.ff_gain)
+            if args.profile != None:
+                profile_path = os.path.join(save_config_path('oversteer'), 'profiles', args.profile + '.ini')
+                defaults = {
+                    'autocenter_strength': 0,
+                    'ff_gain': 0,
+                }
+                config = configparser.ConfigParser(defaults)
+                config.read(profile_path)
+                wheels.set_mode(device_id, config.get('default', 'mode'))
+                wheels.set_range(device_id, int(config.get('default', 'range')))
+                wheels.set_combine_pedals(device_id, True if config.get('default', 'combine_pedals') == 'True' else False)
+                wheels.set_autocenter_strength(device_id, int(config.get('default', 'autocenter_strength')))
+                wheels.set_ff_gain(device_id, int(config.get('default', 'ff_gain')))
 
             sys.exit(0)
 
