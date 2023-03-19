fa33a94578bf6f575fea3e333315c5a6209fc0a3
Alberto Fanjul <albertofanjul@gmail.com>
2023-03-19 11:27:54 +0100

    Latest T150 driver do not need normalization

--- oversteer/device.py
+++ oversteer/device.py
@@ -392,8 +392,6 @@ class Device:
                     event.value = event.value * 64
                 elif self.usb_id in [wid.LG_SFW, wid.LG_MOMO, wid.LG_MOMO2, wid.LG_DF, wid.LG_DFP, wid.LG_DFGT, wid.LG_G25, wid.LG_G27]:
                     event.value = event.value * 4
-                elif self.usb_id in [wid.TM_T150]:
-                    event.value = event.value * 16
             elif self.usb_id in [wid.LG_WFG, wid.LG_WFFG, wid.LG_SFW, wid.LG_MOMO, wid.LG_MOMO2, wid.LG_DF, wid.LG_DFP,
                     wid.LG_DFGT, wid.LG_G920]:
                 if event.code == ecodes.ABS_Y:
