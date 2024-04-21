1e2dac2c3f82ca5244eee1efb5b3224558f50485
yassineimounachen <160498229+yassineimounachen@users.noreply.github.com>
2024-04-21 14:42:33 +0100

    Add Thrustmaster TS-XW Racing Wheel

--- oversteer/device_manager.py
+++ oversteer/device_manager.py
@@ -43,6 +43,7 @@ class DeviceManager:
             wid.TM_T500RS: 1080,
             wid.TM_T80: 240,
             wid.TM_TMX: 900,
+            wid.TM_TSXW: 1080,
             wid.TM_TX: 900,
             wid.XX_FFBOARD: 1080,
         }
